32e847b73f739c6b5ed11338021ab74e62d0ef70
Bhanteja072 <bhanutejapusarla@gmail.com>
2025-10-01 11:49:47 +0530

    Initlized the graph_builder

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -1,3 +1,24 @@
 from langgraph.graph import StateGraph , START, END
 from src.llms.groqllm import GroqLLM
 
+from src.state.blogstate import BlogState
+
+class GraphBuilder:
+    def __init__(self,llm):
+        self.llm = llm
+        self.graph = StateGraph(BlogState)
+
+    def build_topic_graph(self):
+        """
+        Build a graph to generate blog based on topic
+        """
+
+        self.graph.add_node("title",)
+        self.graph.add_node("content",)
+
+        self.graph.add_edge(START, "title")
+        self.graph.add_edge("title", "content")
+        self.graph.add_edge("content", END)
+
+
+        return self.graph
