5d18368bf58550014c9ba06d324a2b6338a4809e
Bhanteja072 <bhanutejapusarla@gmail.com>
2025-10-02 20:12:47 +0530

    Added Graph_builder for language_blog

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -26,9 +26,42 @@ class GraphBuilder:
 
         return self.graph
     
+
+    def build_language_graph(self):
+        """
+        Build a graph to generate blog based on the language
+        """
+
+        self.blog_node_obj = BlogNode(self.llm)
+
+        self.graph.add_node("title",self.blog_node_obj.generate_blog_title)
+        self.graph.add_node("content",self.blog_node_obj.generate_blog_content)
+        self.graph.add_node("route",)
+        self.graph.add_node("hindi_translation",)
+        self.graph.add_node("french_translation",)
+
+
+        #Edges
+        self.graph.add_edge(START, "title")
+        self.graph.add_edge("title", "content")
+        self.graph.add_edge("content","route")
+        self.graph.add_conditional_edges(
+            "route",
+            self.blog_node_obj.route_decision,
+            {
+                "hindi": "hindi_translation",
+                "french": "french_translation"
+            }
+        )
+        self.graph.add_edge("hindi_translation", END)
+        self.graph.add_edge("french_translation", END)
+        return self.graph
+    
     def setup_graph(self,usecase):
         if usecase == "topic":
             self.build_topic_graph()
+        if usecase == "language":
+            self.build_language_graph()
         return self.graph.compile()
 
             
